fe127d052b6ffca10e54422e604924a740344b5c
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-12-09 15:41:06 +0700

    Add files via upload

---
+++ show.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Form Post Demo</title>
+    </head>
+    <body>
+    <strong>สวัสดี: </strong>
+    <?php
+        echo $_POST['name_txt'];
+    ?>
+    </body>
+</html>
