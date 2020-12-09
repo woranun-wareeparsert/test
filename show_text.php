8021e46309d5e9ea17912562a5e6515c28048fc8
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-12-09 17:19:32 +0700

    Update show_text.php

--- show_text.php
+++ show_text.php
@@ -6,7 +6,8 @@
     <body>
     <strong>Diary: </strong>
     <?php
-        echo $_POST['title'];<br>
+        echo $_POST['title'];
+        <br>
         echo $_POST['text'];
     ?>
     </body>
