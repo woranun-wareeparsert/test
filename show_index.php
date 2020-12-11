e7b09c5a0ad1bce48427b85a06616a960695738c
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-12-11 13:51:56 +0700

    Update show_index.php

--- show_index.php
+++ show_index.php
@@ -8,24 +8,23 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <h1 style="text-align:center;">Diary : 
-    <?php
-        echo $_POST['Diary'];
-    ?></h1>
+    <h1 style="text-align:center;">Diary : Diary</h1>
     <style>
-        h1   {font-size:400%; color:white;}
+        h1   {font-size:400%; color:rgb(60, 179, 113);
+        }
+        body {background="wallpaper.jpg"
+        }
+        wrapper{width: 800px;
+        margin: auto;
     </style>
 </head>
 
-<body background="wallpaper.jpg">
-    <h3 style="text-align:center;"><?php echo $_POST['Date'];?><br><br>
-    <?php
-        echo $_POST['Story'];
-    ?><br></h3>
-    <h5 align="right"> Form : 
-    <?php
-        echo $_POST['name'];
-    ?></h5>
+<body>
+    <div id="wrapper">
+        <h3 style="text-align:center;">10/11/2020<br><br>
+        Happy<br></h3>
+        <h5 align="right"> Form : name</h5>
+    </div>
 </body>
 
 </html>
