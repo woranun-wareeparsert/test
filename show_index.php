350ce2b00a75c4bf34c060cdb75f51f63352e9d5
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-12-12 11:03:26 +0700

    Update show_index.php

--- show_index.php
+++ show_index.php
@@ -10,12 +10,9 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <h1 style="text-align:center;">Diary : <?php echo $_POST['Diary'];?></h1>
     <style>
-        h1   {font-size:400%; color:white;
+        h1   {font-size:400%; color:rgb(60, 179, 113);
         }
-        body {background-image: url('wallpaper.jpg');
-              min-height: 100%;
-              background-position: left;
-              background-size: cover;
+        body {background="wallpaper.jpg"
         }
     </style>
 </head>
@@ -23,7 +20,22 @@
 <body>
     <h3 style="text-align:center;"><?php echo $_POST['Date'];?><br><br>
     <?php echo $_POST['Story'];?><br></h3>
-    <h5 align="right"> Form : <?php echo $_POST['name'];?></h5>
-</body>
+    <h5 align="right"> Form : <?php echo $_POST['name'];?></h5><br>
+    <?php echo $_POST['gender'];
+    $text = $_POST['gender'];
+    if ($text == "sad")
+    {
+        echo "สู้ๆ";
+    }
+    elseif ($text == "normal")
+    {
+        echo "ทำต่อไป";
+    }
+    elseif ($text == "happy")
+    {
+        echo "ดีแล้วนะ";
+    }
+    ?>
+    </body>
 
 </html>
